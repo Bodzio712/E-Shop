b031c1ed09834e0cf3993332614076466d2d81ba
pawelwysmyk <pawelwysmyk@gmail.com>
2018-11-07 21:55:09 +0100

    initial.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import flask
+from flask import *
 import sqlite3
 import os
 import werkzeug
@@ -6,6 +6,35 @@ import werkzeug
 app = Flask(__name__)
 app.secret_key = 'ABCD1234'
 
+@app.route("/")
+def root():
+    return render_template('index.html')
+
+@app.route("/rodzaje")
+def root():
+    return render_template('index.html')
+
+@app.route("/producenci")
+def root():
+    return render_template('index.html')
+
+
+@app.route("/kategorie")
+def root():
+    return render_template('index.html')
+
+@app.route("/account/profil")
+def root():
+    return render_template('index.html')
+
+@app.route("/account/orders")
+def root():
+    return render_template('index.html')
+
+@app.route("/account/cart")
+def root():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port= '8080')
