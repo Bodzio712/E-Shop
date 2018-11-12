02a4ce39f749b98a9ae540c49bdaf7c609f723ee
pawelwysmyk <pawelwysmyk@gmail.com>
2018-11-12 11:47:02 +0100

    Poprawa błędu 'database is locked' w sqlite3 i sqlalchemy.

--- main.py
+++ main.py
@@ -7,78 +7,120 @@ import os
 import werkzeug
 from sqlalchemy import *
 from sqlalchemy.ext.automap import *
+from sqlalchemy.pool import StaticPool
 
 app = Flask(__name__)
 app.secret_key = 'ABCD1234'
 
 #dane do navbara
-engine = create_engine("sqlite:///database.db")
+engine = create_engine("sqlite:///database.db",
+                    connect_args={'check_same_thread':False},
+                    poolclass=StaticPool)
 metadata = MetaData(engine, reflect=True)
 
-type = metadata.tables['type']
-manu = metadata.tables['manufacturer']
-cats = metadata.tables['category']
-product = metadata.tables['product']
-cart = metadata.tables['cart']
+def xyz():
+ con = engine.connect()
+ type = metadata.tables['type']
+ manu = metadata.tables['manufacturer']
+ cats = metadata.tables['category']
+ product = metadata.tables['product']
+ cart = metadata.tables['cart']
 
-type_sel = select([type])
-manu_sel = select([manu])
-cats_sel = select([cats])
-product_sel = select([product])
-cart = select([cart])
+ type_sel = select([type])
+ manu_sel = select([manu])
+ cats_sel = select([cats])
+ product_sel = select([product])
+ cart_sel = select([cart])
 
-typeData = engine.execute(type_sel).fetchall()
-manuData = engine.execute(manu_sel).fetchall()
-catData = engine.execute(cats_sel).fetchall()
-productData = engine.execute(product_sel).fetchall()
-cartData = engine.execute(cart).fetchall()
-for row in productData:
-    print(row.productId)
+ typeData = con.execute(type_sel).fetchall()
+ manuData = con.execute(manu_sel).fetchall()
+ catData = con.execute(cats_sel).fetchall()
+ productData = con.execute(product_sel).fetchall()
+ cartData = con.execute(cart_sel)
 
+ con.close()
+ return typeData, manuData, catData
+
+xyz()
+
+def item_number():
+    con = engine.connect()
+    noOfItems = con.execute("SELECT count(productId) FROM cart").fetchone()[0]
+    #engine.dispose()
+    con.close()
+    return noOfItems
 
 @app.route("/")
 def root():
-    return render_template('index.html', categoryData=catData, typeData = typeData, manuData = manuData)
+    item_no = item_number()
+    con = engine.connect()
+    product = metadata.tables['product']
+    meta = select([product])
+    products = con.execute(meta)
+    con.close()
+    typeData, manuData, catData = xyz()
+    return render_template('index.html', categoryData=catData, typeData = typeData, manuData = manuData, noOfItems=item_no, productData=products)
 
 @app.route("/rodzaje")
 def rodzaje():
+    con = engine.connect()
     typeId = request.args.get("typeId")
-#    product = Table('product', metadata, autoload=True, autoload_with=engine)
+    product = Table('product', metadata, autoload=True, autoload_with=engine)
     prod_sel = select([product]).where(product.c.typeId == typeId)
-    prodData = engine.execute(prod_sel).fetchall()
+
+    prodData = con.execute(prod_sel).fetchall()
     print(prodData)
     #x = json.dumps([dict(r) for r in prodData])
     #print(x)
+    #engine.dispose()
+    con.close()
+    typeData, manuData, catData = xyz()
     return render_template('index.html', categoryData=catData, typeData = typeData, manuData = manuData, productData = prodData)
 
 @app.route("/producenci")
 def producenci():
+    con = engine.connect()
     producentId = request.args.get("manuId")
     product = Table('product', metadata, autoload=True, autoload_with=engine)
     prod_sel = select([product]).where(product.c.manufacturerId == producentId)
     prodData = engine.execute(prod_sel).fetchall()
     print(prodData)
+    #engine.dispose()
+    con.close()
+    typeData, manuData, catData = xyz()
     return render_template('index.html', categoryData=catData, typeData=typeData, manuData=manuData,
                            productData=prodData)
 
 @app.route("/kategorie")
 def kategorie():
+    con = engine.connect()
     kategoriaId = request.args.get("catId")
     product = Table('product', metadata, autoload=True, autoload_with=engine)
     prod_sel = select([product]).where(product.c.categoryId == kategoriaId)
     prodData = engine.execute(prod_sel).fetchall()
     print(prodData)
+    #engine.dispose()
+    con.close()
+    typeData, manuData, catData = xyz()
     return render_template('index.html', categoryData=catData, typeData=typeData, manuData=manuData,
                            productData=prodData)
 
-@app.route("/addToCart")
+@app.route("/addToCart", methods=["GET", "POST"])
 def addToCart():
+        for key, value in request.form.items():
+            print("key: {0}, value: {1}".format(key, value))
+        print("QUANTITY:")
+        quant = request.form['liczba']
+        print(quant)
+        con = engine.connect()
         productId = int(request.args.get('productId'))
         clientId = 99
-        cart = Table('cart', metadata, autoload=True, autoload_with=engine)
-        conn = engine.connect()
-        ins = cart.insert().values(clientId = clientId, productId=productId)
-        conn.execute(ins)
+        cart = metadata.tables['cart']
+        ins2 = cart.insert().values(clientId = clientId, productId=productId, quantity=quant)
+        x = con.execute(ins2)
+        #engine.dispose()
+        con.close()
+        return redirect(url_for('root'))
         # commit the record the database
 
 #        with sqlite3.connect('database.db') as conn:
@@ -93,7 +135,7 @@ def addToCart():
 #                conn.rollback()
 #                msg = "Error occured"
 #        conn.close()
-        return redirect(url_for('root'))
+
 
 @app.route("/cart")
 def cart():
@@ -101,17 +143,28 @@ def cart():
 #        return redirect(url_for('loginForm'))
 #    loggedIn, firstName, noOfItems = getLoginDetails()
 #    email = session['email']
-    product = Table('product', metadata, autoload=True, autoload_with=engine)
-    cart = Table('cart', metadata, autoload=True, autoload_with=engine)
+    con = engine.connect()
+    product = metadata.tables['product']
+    cart = metadata.tables['cart']
     meta = select([product, cart], cart.c.productId == product.c.productId)
-    cp_data = engine.execute(meta).fetchall()
-    print(cp_data)
+    cp_data = con.execute(meta).fetchall()
+    #engine.dispose()
+    con.close()
+
+    catData, typeData, manuData = xyz()
     return render_template("index.html", categoryData=catData, typeData=typeData, manuData=manuData,
                            products=cp_data)
 
+def delete_cart():
+ con = engine.connect()
+ d = delete(cart)
+ con.execute(d)
+ engine.dispose()
+ con.close()
 @app.route("/placeOrder")
 def placeOrder():
- with engine.connect() as connection:
+
+  with engine.connect() as connection:
     product = metadata.tables['product']
     cart = metadata.tables['cart']
     orders = metadata.tables['orders']
@@ -136,23 +189,25 @@ def placeOrder():
         valueNet=99.9,
         valueGross=77.8)
         connection.execute(ins)
+  connection.close()
 
-
-    d = delete(cart)
-    engine.execute(d)
-
-    return render_template("index.html")
+  cond = engine.connect()
+  d = delete(cart)
+  cond.execute(d)
+  engine.dispose()
+  cond.close()
+  return render_template("index.html")
 
 
 @app.route("/removeCart")
 def removeFromCart():
-
+    con = engine.connect()
     removeId = request.args.get("productId")
-    cart = Table('cart', metadata, autoload=True, autoload_with=engine)
-    #meta = delete([cart]).where(cart.c.productId == removeId)
-    #cart.query.filter(productId = removeId).delete()
+    cart = metadata.tables['cart']
     d = delete(cart, cart.c.productId == removeId)
-    engine.execute(d)
+    con.execute(d)
+    #engine.dispose()
+    con.close()
     return redirect(url_for('cart'))
 
 @app.route("/tmp")
@@ -161,10 +216,13 @@ def cart2():
 #        return redirect(url_for('loginForm'))
 #    loggedIn, firstName, noOfItems = getLoginDetails()
 #    email = session['email']
+    con = engine.connect()
     product = Table('product', metadata, autoload=True, autoload_with=engine)
     cart = Table('cart', metadata, autoload=True, autoload_with=engine)
     meta = select([product, cart], product.c.productId == cart.c.productId)
-    cp_data = engine.execute(meta).fetchall()
+    cp_data = con.execute(meta).fetchall()
+    con.close()
+    catData, typeData, manuData = xyz()
     return render_template("index.html", categoryData=catData, typeData=typeData, manuData=manuData,
                            products=cp_data)
 
@@ -175,18 +233,20 @@ def accountProfil():
 
 @app.route("/account/orders")
 def accountOrders():
+    con = engine.connect()
     orders = metadata.tables['orders']
     ord_sel = select([orders])
 
-    productData = engine.execute(ord_sel).fetchall()
+    productData = con.execute(ord_sel).fetchall()
     for row in productData:
         print(row)
-
+    #engine.dispose()
+    con.close()
     return render_template("index.html", orders=productData)
 
 
 
-@app.route("/registerForm")
+@app.route("/registerForm", methods = ['GET', 'POST'])
 def registerForm():
     return render_template('register.html')
 
@@ -195,6 +255,11 @@ def registerForm():
 def loginForm():
     return render_template('login.html')
 
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('root'))
 
 if __name__ == '__main__':
     app.run(debug=True, port= '8080')
+    #engine.dispose()
