b618b2775d311679ab9c33e31c20552a34968be6
pawelwysmyk <pawelwysmyk@gmail.com>
2018-11-14 19:52:32 +0100

    Wygląd listy produktów i logika.

--- static/js/main.js
+++ static/js/main.js
@@ -32,28 +32,6 @@ $(document).ready(function() {
   updateSumItems();
 });
 
-$('.promo-code-cta').click(function() {
-
-  promoCode = $('#promo-code').val();
-
-  if (promoCode == '10off' || promoCode == '10OFF') {
-    //If promoPrice has no value, set it as 10 for the 10OFF promocode
-    if (!promoPrice) {
-      promoPrice = 10;
-    } else if (promoCode) {
-      promoPrice = promoPrice * 1;
-    }
-  } else if (promoCode != '') {
-    alert("Invalid Promo Code");
-    promoPrice = 0;
-  }
-  //If there is a promoPrice that has been set (it means there is a valid promoCode input) show promo
-  if (promoPrice) {
-    $('.summary-promo').removeClass('hide');
-    $('.promo-value').text(promoPrice.toFixed(2));
-    recalculateCart(true);
-  }
-});
 
 /* Recalculate cart */
 function recalculateCart(onlyTotal) {
@@ -67,16 +45,6 @@ function recalculateCart(onlyTotal) {
   /* Calculate totals */
   var total = subtotal;
 
-  //If there is a valid promoCode, and subtotal < 10 subtract from total
-  var promoPrice = parseFloat($('.promo-value').text());
-  if (promoPrice) {
-    if (subtotal >= 10) {
-      total -= promoPrice;
-    } else {
-      alert('Order must be more than £10 for Promo code to apply.');
-      $('.summary-promo').addClass('hide');
-    }
-  }
 
   /*If switch for update only total, update only total display*/
   if (onlyTotal) {
