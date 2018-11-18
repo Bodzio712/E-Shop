1696e5530e9212e0fc05acb72a72714373d51a04
Piotr Pogodziński <284953@pw.edu.pl>
2018-11-18 22:53:06 +0100

    Werfikacja wprowadzanych danych rejestracji w JS

--- static/js/registerForm.js
+++ static/js/registerForm.js
@@ -1,5 +1,5 @@
 function validate() {
-    if(validate_passwords() == True) {
+    if((validate_passwords() == true) && (validate_phone() == true) && (validate_email() == true)) {
         return true;
     } else {
         return false;
@@ -7,7 +7,7 @@ function validate() {
 }
 
 function validate_passwords() {
-        var pass = document.getElementById("password").value;
+    var pass = document.getElementById("password").value;
     var cpass = document.getElementById("cpassword").value;
     if (pass == cpass) {
         return true;
@@ -18,5 +18,24 @@ function validate_passwords() {
 }
 
 function validate_phone() {
+    var regex = /^\d{9}$/;
+    var phone = document.getElementById("phone").value.toString();
+    if(regex.test(phone) == true) {
+        return true;
+    } else {
+        alert("Podaj numer telefonu w formacie: 111222333");
+        return false;
+    }
 
+}
+
+function validate_email() {
+    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm
+    var email = document.getElementById("email").valueOf.toString();
+        if(regex.test(email) == true) {
+        return true;
+    } else {
+            alert("Adres email ma niepoprawny format");
+            return false;
+    }
 }
